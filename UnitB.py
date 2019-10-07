227150cede85e71b73a12a874e69199506bf1c41
Sgtcarter <47240556+Sgtcarter@users.noreply.github.com>
2019-10-07 16:47:08 -0700

    Create UnitB.py

---
+++ UnitB.py
@@ -0,0 +1,15 @@
+#String Type Test
+
+
+
+print("Please enter in a string:")
+string = input("")
+print(string.isupper())
+print(string.isdigit())
+print(string.isalpha())
+
+
+#Escape Characters with a string
+
+haiku = "Type, type, type away.\nCompile. Run. Hip hip hooray!\n No error today!"
+print(haiku)
